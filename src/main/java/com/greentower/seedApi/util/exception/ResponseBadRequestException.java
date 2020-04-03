109302ce1dfb512af8ab831c67267214717dda20
Gabriel Mandelli <gabriel-mandelli@hotmail.com>
2020-04-03 12:56:35 -0300

    Update Response Exception

--- src/main/java/com/greentower/seedApi/util/exception/ResponseBadRequestException.java
+++ src/main/java/com/greentower/seedApi/util/exception/ResponseBadRequestException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-public class ValidationBadRequestException extends RuntimeException {
-    public ValidationBadRequestException(String message){
+public class ResponseBadRequestException extends RuntimeException {
+    public ResponseBadRequestException(String message){
         super(message);
     }
 }
--- src/main/java/com/greentower/seedApi/util/exception/ResponseNotFoundException.java
+++ src/main/java/com/greentower/seedApi/util/exception/ResponseNotFoundException.java
@@ -4,8 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResponseNotFoundException extends Exception {
-    public ResponseNotFoundException(String message) {
-        super(message);
-    }
+public class ResponseNotFoundException extends RuntimeException {
+    public ResponseNotFoundException(String message) { super(message); }
 }
